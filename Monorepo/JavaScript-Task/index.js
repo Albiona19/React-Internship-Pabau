7ac30a9116717583435512c6bfc8ffec5e82be89
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-09-09 14:23:47 +0300

    Albiona 2001

--- Monorepo/JavaScript-Task/index.js
+++ Monorepo/JavaScript-Task/index.js
@@ -5,30 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
   const showButton = document.getElementById("showButton");
   const selectedValuesDiv = document.getElementById("selectedValues");
 
-  shuffleButton.addEventListener("click", shuffleValues);
+  shuffleButton.addEventListener("click", shuffleLabels); 
   changeButton.addEventListener("click", changeValues);
   showButton.addEventListener("click", showSelectedValues);
 
-  function shuffleValues() {
-    const checkboxContainer = document.getElementById("checkboxForm");
-    const checkboxesArray = Array.from(
-      checkboxContainer.getElementsByTagName("input")
-    );
-    for (let i = checkboxesArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [checkboxesArray[i], checkboxesArray[j]] = [
-        checkboxesArray[j],
-        checkboxesArray[i],
-      ];
-    }
-    checkboxesArray.forEach((checkbox, index) => {
-      checkboxContainer.appendChild(checkbox);
+  function shuffleLabels() { 
+    const labelValues = Array.from(checkboxes).map((checkbox) => checkbox.value);
+    const shuffledLabelValues = shuffleArray(labelValues);
+
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.nextElementSibling.textContent = shuffledLabelValues[index];
     });
+  }
+
+  function shuffleArray(array) { 
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
   function changeValues() {
     checkboxes.forEach((checkbox) => {
-      checkbox.value = "NewValue" + (Math.random() * 100).toFixed(0);
+      checkbox.value = "Value changed";
     });
   }
 
