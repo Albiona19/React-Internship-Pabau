2b540f12c123263dac3f8a671dfd279d80afef90
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-09-09 15:10:14 +0300

    Albiona2001

--- Monorepo/react-folder-task/src/Components/Footer.js
+++ Monorepo/react-folder-task/src/Components/Footer.js
@@ -3,9 +3,14 @@ import React from 'react';
 function Footer({ handleBackClick }) {
   return (
     <footer className="footer">
-      <button className="back-button" onClick={handleBackClick}>
-        Back
-      </button>
+      <div className="footer-content">
+        <button className="back-button" onClick={handleBackClick}>
+          Back
+        </button>
+      </div>
+        <div className="footer-text">
+          Not sure consultation type? Please call 02037959063, Powered by Pabau
+        </div>
     </footer>
   );
 }
